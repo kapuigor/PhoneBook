0f1fcd1801952cd063d1ace6867399cd93918abb
Igor Kapustin <kapui-igor@ya.ru>
2021-09-21 20:31:25 +0300

    Launch screen Row height App icon

--- PhoneBook/MainViewController.swift
+++ PhoneBook/MainViewController.swift
@@ -33,8 +33,16 @@ class MainViewController: UITableViewController {
 
         cell.textLabel?.text = contacts[indexPath.row]
         cell.imageView?.image = UIImage(named: contacts[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         
         return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
 
     /*
