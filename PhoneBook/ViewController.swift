a1c1f331e65c2913bfa32ae35dcee065932b9a0f
Igor Kapustin <kapui-igor@ya.ru>
2021-09-21 09:09:47 +0300

    Initial Commit

---
+++ PhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneBook
+//
+//  Created by Игорь Капустин on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
