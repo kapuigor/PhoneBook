eac0d0769403839ee128474c433a561a7994f45f
Igor Kapustin <kapui-igor@ya.ru>
2021-09-21 11:21:14 +0300

    Initial

---
+++ PhoneBook/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  PhoneBook
+//
+//  Created by Игорь Капустин on 21.09.2021.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let contacts = [
+        "Ivan" , "Alexey", "Fedor",
+        "Stepan", "Tatyana", "Nikolay"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = contacts[indexPath.row]
+        cell.imageView?.image = UIImage(named: contacts[indexPath.row])
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
